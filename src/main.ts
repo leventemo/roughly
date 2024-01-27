a56baa4873299e05b07005f81a5b7c8e21eb597d
leventemo <leventeamolnar@gmail.com>
2024-01-27 07:58:09 +0100

    move svg icons into assets folder

--- src/main.ts
+++ src/main.ts
@@ -23,15 +23,16 @@ window.onload = function () {
     <div id="msg" class="invisible"></div>
     <div class="content-box">
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" viewBox="0 -2 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-        </svg><span id='question'> question</span>
+         <img src='./assets/questionmark.svg' alt="some file"  height='20' width='20'/>
+        <span id='question'> question</span>
       </div>
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" viewBox="0 -2 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        </svg><span id='answer'> answer</span>
+        <img src='./assets/tick.svg' alt="some file"  height='20' width='20'/>
+        <span id='answer'> answer</span>
       </div>
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="20" viewBox="0 -2 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" /></svg><span id='link'> background info</span>
+        <img src='./assets/book.svg' alt="some file"  height='20' width='20'/>
+        <span id='link'> background info</span>
       </div>
     </div>
 
